44e7484df72b271c0662c4f5f37dceca01cbe702
BonesetterB <122158929+BonesetterB@users.noreply.github.com>
2023-07-08 15:32:23 +0300

    x

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import codecs
 #https://quotes.toscrape.com/page/10/
 
-def  search():
-    url = 'http://quotes.toscrape.com'
+def  search(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
-    while True:
-        try:
-            all_qoute=soup.find_next_sibling("div",class_='quote')
+    for i in range(2):
+            all_qoute=soup.find("div",class_='quote')
             qoute=all_qoute.find('span')
             tags=all_qoute.find_all('a',class_='tag')
             tag_texts = [tag.text for tag in tags]
             autor=all_qoute.find('small')
-            with open('data.json', 'w') as file:
-                json.dump("json/quotes.json", file)
-            print(qoute.text)
-            print(tag_texts)
-            print(autor.text)
-        except:
-            break
+            data={"tags":tag_texts,
+                  "author":autor.text,
+                  "quote":qoute.text}
+            with open('json/quotes.json', 'w') as file:
+                json.dump(data, file)
+                
     return None
 
 if __name__=='__main__':
-    print(search())
+    for i in range(1,2):
+        url=f'https://quotes.toscrape.com/page/{i}/'
+        search(url)
